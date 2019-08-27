1d381da613aa08a02f88704e5ffa470cc83e641a
Zer0 <lucismcg@gmail.com>
2019-08-27 08:01:51 -0600

    removing a missed consol log

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -51,7 +51,6 @@ $("#searchA").on("click", function(event) {
     event.preventDefault();
     results = $("#searchId").val();
     queryURL = 'https://api.edamam.com/search?q=' + results + '&app_id=' + APIId + '&app_key=' + APIKey + '&from=0&to=10&calories=591-722&time=30';
-    console.log("search: " + results);
     ajaxSearch();
     $("#introDiv").empty();
 
